d7bacef25fc59249df731d60ee4acf4f6ae042c5
kathy789 <ji79@purdue.edu>
2016-11-18 17:37:23 -0500

    add .gitignore

--- CSCapstone/urls.py
+++ CSCapstone/urls.py
@@ -19,10 +19,9 @@ urlpatterns = [
     url(r'^', include('CompaniesApp.urls')),
     url(r'^', include('GroupsApp.urls')),
 	url(r'^', include('UniversitiesApp.urls')),
-<<<<<<< HEAD
+
     url(r'^', include('CommentsApp.urls')),
     url(r'^', include('TeacherApp.urls')),
-=======
 
->>>>>>> master
+
 ]
