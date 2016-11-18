7ff0064b22d3f60ee2ce0e0dbce748916c78a43d
victor lee <lee1961@purdue.edu>
2016-11-18 04:44:47 -0500

    now only users who are engineers can add

--- ProjectsApp/views.py
+++ ProjectsApp/views.py
@@ -11,6 +11,8 @@ from .models import Project
 
 def getProjects(request):
 	projects_list = models.Project.objects.all()
+
+
 	return render(request, 'projects.html', {
         'projects': projects_list,
     })
@@ -21,21 +23,27 @@ def getProject(request):
 def addProject(request):
 	form_class = ProjectForm
 
-	if request.method == 'POST':
-		form = form_class(data=request.POST)
+	user_status = request.user.is_engineer
+	if user_status == True:
+		if request.method == 'POST':
+			form = form_class(data=request.POST)
 
-		if form.is_valid():
-			#print("i am here")
-			name = request.POST.get('name')
-			description = request.POST.get('description')
-			time = timezone.now()
+			if form.is_valid():
+				#print("i am here")
+				name = request.POST.get('name')
+				description = request.POST.get('description')
+				time = timezone.now()
 
-			new_project = Project(name=name,description=description,created_at=time,updated_at=time)
-			new_project.save()
+				new_project = Project(name=name,description=description,created_at=time,updated_at=time)
+				new_project.save()
 
+				return redirect('project:Projects')
+		else:
+			print("asdas")
 			return redirect('project:Projects')
 
-	return render(request,'addproject.html', {
-		'form' : form_class
-	})
+	#this person is not even
+	else:
+		return redirect('project:Projects')
+
 
