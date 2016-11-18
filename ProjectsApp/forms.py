36158b4e7d8dd57f60576a6a1ff7873af47bdba3
victor lee <lee1961@purdue.edu>
2016-11-18 04:28:23 -0500

    added projects and can submit but still doesnt control user

--- ProjectsApp/forms.py
+++ ProjectsApp/forms.py
@@ -4,6 +4,6 @@ from django import forms
 class ProjectForm(forms.Form):
     name = forms.CharField(required=True,max_length=200)
     description = forms.CharField(max_length=10000,required=True)
-    created_at = forms.DateTimeField('date created')
-    updated_at = forms.DateTimeField('date updated')
+    # created_at = forms.DateTimeField('date created')
+    # updated_at = forms.DateTimeField('date updated')
 
--- ProjectsApp/views.py
+++ ProjectsApp/views.py
@@ -2,10 +2,12 @@
 
 Created by Harris Christiansen on 10/02/16.
 """
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import ProjectForm
+from django.utils import timezone
 
 from . import models
+from .models import Project
 
 def getProjects(request):
 	projects_list = models.Project.objects.all()
@@ -18,7 +20,21 @@ def getProject(request):
 
 def addProject(request):
 	form_class = ProjectForm
-	
+
+	if request.method == 'POST':
+		form = form_class(data=request.POST)
+
+		if form.is_valid():
+			#print("i am here")
+			name = request.POST.get('name')
+			description = request.POST.get('description')
+			time = timezone.now()
+
+			new_project = Project(name=name,description=description,created_at=time,updated_at=time)
+			new_project.save()
+
+			return redirect('project:Projects')
+
 	return render(request,'addproject.html', {
 		'form' : form_class
 	})
