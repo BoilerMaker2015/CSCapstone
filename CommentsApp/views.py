2d883f66399106567c2b58433cc05eb7f1502537
kathy789 <ji79@purdue.edu>
2016-12-06 16:50:55 -0500

    add comment to group

--- CommentsApp/views.py
+++ CommentsApp/views.py
@@ -2,19 +2,28 @@ from . import forms
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Comment,SubComment
+from GroupsApp.models import Group
 #from . import forms
 from django.core.urlresolvers import reverse_lazy
 
 
 # Create your views here.
-def getComments(request):
+def getComments(request, group_id):
+    if request.user.is_authenticated():
+        comment_list = Comment.objects.all()   # the problem is we could not find the comments only for that group, so we add a ManytoManyfiled in group, which is comments.
+        myGroup = Group.objects.get(pk=group_id)
+        is_member =myGroup.members.filter(email__exact=request.user.email)
+        context = {
+            'comment' : comment_list,
+            'group': myGroup,
+            'userIsMember': is_member
+        }
+        return render(request,'comments.html',context)   
+        #return HttpResponse("Hello, world. You're at the polls index.")
+    else:
+        return render(request, 'autherror.html')
 
-    comment_list = Comment.objects.all()
-    context = {
-        'comment' : comment_list,
-    }
-    return render(request,'comments.html',context)
-    #return HttpResponse("Hello, world. You're at the polls index.")
+
 
 def getCommentForm(request):
     return render(request, 'commentForm.html')
