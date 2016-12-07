e88f3dd73154e4f8f30f04cef871b0a36f09bc52
victor lee <lee1961@purdue.edu>
2016-12-07 03:36:37 -0500

    fixed some constraint

--- CSCapstoneApp/views.py
+++ CSCapstoneApp/views.py
@@ -5,6 +5,8 @@ Created by Harris Christiansen on 9/18/16.
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def getIndex(request):
 	return render(request, 'index.html', {
         'foo': 'bar',
