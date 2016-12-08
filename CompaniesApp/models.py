59dddba5498084f8dfa9fdcb4d6dff374ca529e6
Wentai Cui <wentaicui@Yahoo.com>
2016-12-08 12:57:05 -0500

    Update models.py

--- CompaniesApp/models.py
+++ CompaniesApp/models.py
@@ -7,12 +7,7 @@ from django.db import models
 from AuthenticationApp.models import MyUser
 
 # Create your models here.
-class Company(models.Model):
-    name = models.CharField(max_length=30)
-    photo = models.ImageField(upload_to="static/companyimages", default=0)
-    description = models.CharField(max_length=300)
-    website=models.CharField(max_length=300, default="/")
-    members = models.ManyToManyField(MyUser)
+
     
     def __str__(self):
         return self.name
