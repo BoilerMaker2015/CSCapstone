8b8fa4fc1db5d8305c7116c6fe297688dcc38b57
Wentai Cui <wentaicui@Yahoo.com>
2016-12-08 12:38:41 -0500

    Update forms.py

--- TeacherApp/forms.py
+++ TeacherApp/forms.py
@@ -2,7 +2,8 @@ from django import forms
 class TeacherForm(forms.Form):
     first_name = forms.CharField(label='first_name', max_length=120)
     last_name = forms.CharField(label='last_name', max_length=120)
-
+    university = forms.CharField(label='university', max_length=120)
+    phone = forms.CharField(label='phone', max_length=10)
 class TeachClassForm(forms.Form):  
 	title = forms.CharField(
         max_length = 120
